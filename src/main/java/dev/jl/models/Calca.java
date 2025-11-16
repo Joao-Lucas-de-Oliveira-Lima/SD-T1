58b06054ee30a7dea48dae226205908bdc7b2de5
Jeudess <155033448+Jeudess@users.noreply.github.com>
2025-11-16 09:09:08 -0300

    Remove arquivos para iniciar trabalho2

--- src/main/java/dev/jl/models/Calca.java
+++
@@ -1,9 +0,0 @@
-package dev.jl.models;
-
-import java.io.Serializable;
-
-public class Calca extends PecaRoupa implements Serializable {
-    public Calca(int id, String nome, double preco, String tamanho, String cor, int quantidadeEstoque) {
-        super(id, nome, preco, tamanho, cor, quantidadeEstoque);
-    }
-}
--- src/main/java/dev/jl/models/Camiseta.java
+++
@@ -1,9 +0,0 @@
-package dev.jl.models;
-
-import java.io.Serializable;
-
-public class Camiseta extends PecaRoupa implements Serializable {
-    public Camiseta(int id, String nome, double preco, String tamanho, String cor, int quantidadeEstoque) {
-        super(id, nome, preco, tamanho, cor, quantidadeEstoque);
-    }
-}
